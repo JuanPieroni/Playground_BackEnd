4700c407ef6a0fe8f1bf18839ca5d7815df4b254
Alejandro Di Stefano <addistefano@live.com.ar>
2025-06-02 21:55:03 -0300

    Clase 01

---
+++ index.js
@@ -0,0 +1,105 @@
+// Conceptos basicos
+
+// Variables
+
+// var ciudad = 'Buenos Aires';
+// console.log(ciudad);
+// var ciudad = 5;
+// console.log(ciudad);
+// var ciudad = [1,2];
+// console.log(ciudad);
+// // ------------------
+// let variableA = 'Alejandro';
+// console.log(variableA);
+// // Reasignamos un valor
+// variableA = 'Di Stefano';
+// console.log(variableA);
+// // Reasignamos un valor
+// variableA = 5;
+// console.log(variableA);
+// // Reasignamos un valor
+// variableA = [1,2,3];
+// console.log(variableA);
+// // ------------------
+
+// const constanteA = 6;
+// console.log(constanteA);
+// // No podemos Reasignar otro valor !!!!!!!!!!!!!!!!!!!!!
+// // constanteA = 0;
+// // constanteA = 'String';
+
+// Codicional
+// let edad = 16;
+// if (edad >= 18) {
+//     console.log("Usted es mayor")
+// } else {
+//     console.warn("Usted es menor")
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log("El valor de i es: " + i);
+// }
+
+// Stack MERN  (Mongo, Express, React, Node)
+// Stack MEAN  (Mongo, Express, Angular, Node)
+// Stack MEVN  (Mongo, Express, Vue, Node)
+// Stack MENN  (Mongo, Express, Nest, Node)
+
+
+// Funcion 
+
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+// console.log(sumar(3,4)); // 7
+// console.log(sumar(3,7)); // 10
+
+// const sumar = function (a, b) {
+//     return a + b;
+// }
+
+// console.log(sumar(3, 4)); // 7
+
+
+// const sumar = (a, b) => a + b;
+// console.log(sumar(3, 4)); // 7
+
+// const resultado = (function(a,b){
+//     return a + b;
+// })(3,4);
+
+// console.log(resultado); // 7
+
+// const calculadora = {
+//     sumar: function(a,b){
+//         return a + b;
+//     }
+// };
+
+// console.log(calculadora.sumar(3,4));
+
+// const calculadora = {
+//     sumar(a, b) {
+//         return a + b;
+//     }
+// };
+// console.log(calculadora.sumar(3, 4));
+
+// class Calculadora {
+//     static sumar(a,b){
+//         return a + b;
+//     }
+// }
+
+// console.log(Calculadora.sumar(4,3));
+
+// function Calculadora() {}
+
+// Calculadora.prototype.sumar = function(a,b){
+//     return a + b;
+// };
+
+// const miCalc = new Calculadora();
+// console.log(miCalc.sumar(3,4));
+
