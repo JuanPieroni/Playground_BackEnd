8984fe03f909ccd32cae777b777a292bcf59b17b
Juan Pablo Pieroni <seisduro@gmail.com>
2025-06-03 23:08:17 -0300

    app.use(express.json())

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const app = express();
-const PORT = 3000; // Cualquier puerto a eleccion
+const express = require("express")
+const app = express()
+app.use(express.json())
+const PORT = 3000 // Cualquier puerto a eleccion
 
 // Ruta de prueba
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("¡Estoy escuchando tu solicitud! ✅​")
 })
 
-app.get('/consulta', (req, res) => {
+app.get("/consulta", (req, res) => {
+    res.send("¡Estoy escuchando tu solicitud en la ruta /consulta! ✅​")
+})
+
+app.get("/consultaJson", (req, res) => {
     res.json({
-        message: "¡Estoy escuchando tu solicitud en la ruta /consulta! ✅​"
+        status: "Estoy escuchando tu solicitud en la ruta /consultaJson! ✅​",
     })
 })
 
@@ -17,3 +22,4 @@ app.get('/consulta', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en: http://localhost:${PORT}`)
 })
+ 
